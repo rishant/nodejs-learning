75a9b91d6983224c52c4786e17c5c5542325e54a
rishantgupta <rishantgupta007@gmail.com>
2022-12-11 20:03:58 +0530

    1. Textbox value read on Textbox value change to update in Paragraph. 2. Count increase on every-time Button click as show in Paragraph.

--- 5NodejsReact/react-app-example1/src/classcomponents/AppClassComponent.js
+++ 5NodejsReact/react-app-example1/src/classcomponents/AppClassComponent.js
@@ -19,8 +19,12 @@ class AppClassComponent extends React.Component {
 
     // Note: Read data by element Id. Id should be always unique.
     let inputbox = document.getElementById("input_name_2");
-    this.setState({data: inputbox.value, count: this.state.count+1});
+    this.setState({data: inputbox.value});
     return;
+  }
+
+  updateClickCount2(){
+    this.setState({count: this.state.count+1});
   }
 
   render() {
@@ -30,11 +34,13 @@ class AppClassComponent extends React.Component {
         <header className="AppCC-header">
           <img src={logo} className="AppCC-logo" alt="logo" />
           <h1 style={{ color: "red" }}>Hello Style!</h1>
-          <p>Class state data: {this.state.data}</p>
-          <p>Class Update Count: {this.state.count}</p>
           <span>
             <input type="text" id='input_name_2' name="input_name_2" style={{ margin: "5px"}} onChange={() => {this.updateDataOnClick2()}} />
-            {/* <button onClick={() => {this.updateDataOnClick2()}}>Click me !</button> */}
+            <p>Class state data: {this.state.data}</p>
+          </span>
+          <span>
+            <button onClick={() => {this.updateClickCount2()}}>Click me !</button>
+            <p>Class Update Count: {this.state.count}</p>
           </span>
         </header>
       </div>
--- 5NodejsReact/react-app-example1/src/functionalcomponents/App.js
+++ 5NodejsReact/react-app-example1/src/functionalcomponents/App.js
@@ -17,8 +17,12 @@ function App(props) {
     // Note: Read data by element Id. Id should be always unique.
     let inputbox = document.getElementById("input_name_1");
     setData(inputbox.value);
+  }
+  
+  function updateClickCount1(){
     setCount(count+1);
   }
+
   console.log("-----------updateDataOnClick function component----------");
   
   return (
@@ -26,11 +30,13 @@ function App(props) {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 style={{ color: "red" }}>Hello Style!</h1>
-          <p>Function state data: {data}</p>
-          <p>Function update Count: {count}</p>
           <span>
             <input type="text" id='input_name_1' name="input_name_1" style={{ margin: "5px"}} onChange={() => {updateDataOnClick1()}} />
-            {/* <button onClick={() => {updateDataOnClick1()}}>Click me !</button> */}
+            <p>Function state data: {data}</p>
+          </span>
+          <span>
+            <button onClick={() => {updateClickCount1()}}>Click me !</button>
+            <p>Function update Count: {count}</p>
           </span>
         </header>
       </div>
