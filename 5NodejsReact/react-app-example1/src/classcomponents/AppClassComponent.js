153bec108e68655e130f5b5e350db65ceba28b49
rishantgupta <rishantgupta007@gmail.com>
2022-12-12 15:40:59 +0530

    React Class Component Lifecycle method and workflow

--- 5NodejsReact/react-app-example1/src/classcomponents/AppClassComponent.js
+++ 5NodejsReact/react-app-example1/src/classcomponents/AppClassComponent.js
@@ -6,6 +6,11 @@ class AppClassComponent extends React.Component {
 
   constructor(props) {
     super();
+    console.log(`
+      constructor Class Component:
+        Description :: This is the 1st method which will call once while creating the component.
+        Here we are Initialize default values of variables or getting parent props`
+    )
     this.state = {
       data: "Hello",
       count: 0
@@ -28,7 +33,15 @@ class AppClassComponent extends React.Component {
   }
 
   render() {
-    console.log("-----------updateDataOnClick Class Component----------");
+    console.log(`
+      Render Class Component:
+        Description :: This is the 2nd method which will call once while constructor done.
+        Here we are Preprating HTML Template using Initialed variables value.`
+    )
+
+    // going infinate loop. We should not set state vaue into render() method.
+    // this.setState({ count : this.state.count+3})
+
     return (
       <div className="AppCC">
         <header className="AppCC-header">
@@ -45,6 +58,45 @@ class AppClassComponent extends React.Component {
         </header>
       </div>
     );
+  }
+
+  componentDidMount() {
+    console.log(`
+      ComponentDidMount Class Component:
+        Description :: This is the 3rd method which will call to FETCH data and set in local variable or State.
+        Component will not Reload on Set/Update values of local Variables | PROPS | STATE.
+        1. Pre Fetch API Calls for initial value set.
+    `)
+  }
+  
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(`
+      ComponentDidUpdate Class Component:
+        Description :: This method will call everytime before render() method. 
+        Here we should update Component STATE conditional based else it will go in INFINATE Loop.
+    `)
+  }
+  
+  shouldComponentUpdate() {
+    console.log(`
+      ShouldComponentUpdate Class Component:
+        Description :: This method is used for CONTROL componentDidUpdate() using condition and based on condition we return True/False.
+    `)
+    return true;
+  }
+  
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate Class Component")
+    return null;
+  }
+  
+  componentWillUnmount() {
+    console.log(`
+      ComponentWillUnmount Class Component:
+        Description :: This method will called on component removed from DOM.
+        Example -> If we Show/Hide component then on Hide component this method will call not on Show component.
+    
+    `)
   }
 }
 
